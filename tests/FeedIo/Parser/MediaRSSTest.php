e78db7ee1e13f4f84072212cd570d5f7003c3b4a
Éloi Rivard <eloi@yaal.fr>
2019-12-10 16:48:44 +0100

    Tag priority test

--- tests/FeedIo/Parser/MediaRSSTest.php
+++ tests/FeedIo/Parser/MediaRSSTest.php
@@ -15,6 +15,7 @@ use FeedIo\Parser\XmlParser as Parser;
 use FeedIo\Reader\Document;
 use FeedIo\Rule\DateTimeBuilder;
 use FeedIo\Standard\Atom;
+use FeedIo\Standard\Rss;
 use Psr\Log\NullLogger;
 
 use \PHPUnit\Framework\TestCase;
@@ -27,7 +28,7 @@ class MediaRssTest extends TestCase
      * @param $filename
      * @return Document
      */
-    protected function buildDomDocument($filename)
+    protected function buildDomDocumentFromFile($filename)
     {
         $file = dirname(__FILE__)."/../../samples/{$filename}";
         $domDocument = new \DOMDocument();
@@ -38,7 +39,7 @@ class MediaRssTest extends TestCase
 
     public function testYoutubeFeed()
     {
-        $document = $this->buildDomDocument(static::SAMPLE_FILE);
+        $document = $this->buildDomDocumentFromFile(static::SAMPLE_FILE);
         $standard = new Atom(new DateTimeBuilder());
         $parser = new Parser($standard, new NullLogger());
         $feed = $parser->parse($document, new Feed());
@@ -53,6 +54,125 @@ class MediaRssTest extends TestCase
             $this->assertEquals('https://i2.ytimg.com/vi/YT_VIDEO_ID/hqdefault.jpg', $media->getThumbnail());
             $this->assertEquals("This is a\nmultiline\ndescription", $media->getDescription());
             $this->assertEquals('https://www.youtube.com/v/YT_VIDEO_ID?version=3', $media->getUrl());
+        }
+    }
+
+    protected function buildDomDocumentFromXML($xml)
+    {
+        $document = new Document($xml);
+        $standard = new Rss(new DateTimeBuilder());
+        $parser = new Parser($standard, new NullLogger());
+        $feed = $parser->parse($document, new Feed());
+        return $feed;
+    }
+    /**
+     * From http://www.rssboard.org/media-rss#optional-elements
+     *
+     * Duplicated elements appearing at deeper levels of the document tree
+     * have higher priority over other levels. For example, <media:content>
+     * level elements are favored over <item> level elements. The priority
+     * level is listed from strongest to weakest:
+     * <media:content>, <media:group>, <item>, <channel>.
+     */
+    public function testTagsPriority()
+    {
+        $xml_media_content_description = '
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Title of page</title>
+                    <link>http://www.foo.com</link>
+                    <description>Description of page</description>
+                    <item>
+                        <title>Story about something</title>
+                        <link>http://www.foo.com/item1.htm</link>
+                        <media:group>
+                            <media:content url="http://www.foo.com/trailer.ogg">
+                                <media:description>Description from media:content</media:description>
+                            </media:content>
+                            <media:description>Description from media:group</media:description>
+                        </media:group>
+                        <media:description>Description from item</media:description>
+                    </item>
+                    <media:description>Description from channel</media:description>
+                </channel>
+            </rss>';
+
+        $feed_media_content_description = $this->buildDomDocumentFromXML($xml_media_content_description);
+        foreach ($feed_media_content_description as $item) {
+            $media = $item->getMedias()->current();
+            $this->assertEquals('Description from media:content', $media->getDescription());
+        }
+
+        $xml_media_group_description = '
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Title of page</title>
+                    <link>http://www.foo.com</link>
+                    <description>Description of page</description>
+                    <item>
+                        <title>Story about something</title>
+                        <link>http://www.foo.com/item1.htm</link>
+                        <media:group>
+                            <media:content url="http://www.foo.com/trailer.ogg" />
+                            <media:description>Description from media:group</media:description>
+                        </media:group>
+                        <media:description>Description from item</media:description>
+                    </item>
+                    <media:description>Description from channel</media:description>
+                </channel>
+            </rss>';
+
+        $feed_media_group_description = $this->buildDomDocumentFromXML($xml_media_group_description);
+        foreach ($feed_media_group_description as $item) {
+            $media = $item->getMedias()->current();
+            $this->assertEquals('Description from media:group', $media->getDescription());
+        }
+
+        $xml_item_description = '
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Title of page</title>
+                    <link>http://www.foo.com</link>
+                    <description>Description of page</description>
+                    <item>
+                        <title>Story about something</title>
+                        <link>http://www.foo.com/item1.htm</link>
+                        <media:group>
+                            <media:content url="http://www.foo.com/trailer.ogg" />
+                        </media:group>
+                        <media:description>Description from item</media:description>
+                    </item>
+                    <media:description>Description from channel</media:description>
+                </channel>
+            </rss>';
+
+        $feed_item_description = $this->buildDomDocumentFromXML($xml_item_description);
+        foreach ($feed_item_description as $item) {
+            $media = $item->getMedias()->current();
+            $this->assertEquals('Description from item', $media->getDescription());
+        }
+
+        $xml_channel_description = '
+            <rss version="2.0" xmlns:media="http://search.yahoo.com/mrss/">
+                <channel>
+                    <title>Title of page</title>
+                    <link>http://www.foo.com</link>
+                    <description>Description of page</description>
+                    <item>
+                        <title>Story about something</title>
+                        <link>http://www.foo.com/item1.htm</link>
+                        <media:group>
+                            <media:content url="http://www.foo.com/trailer.ogg" />
+                        </media:group>
+                    </item>
+                    <media:description>Description from channel</media:description>
+                </channel>
+            </rss>';
+
+        $feed_channel_description = $this->buildDomDocumentFromXML($xml_channel_description);
+        foreach ($feed_channel_description as $item) {
+            $media = $item->getMedias()->current();
+            $this->assertEquals('Description from channel', $media->getDescription());
         }
     }
 }
